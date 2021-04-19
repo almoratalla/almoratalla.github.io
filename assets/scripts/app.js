e6b48ba107b68b19cc775905453741344a15493e
Alain Moratalla <realmoratalla@gmail.com>
2021-04-19 15:29:42 +0800

    added initial look for landing page
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

---
+++ assets/scripts/app.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+eval("console.log('Heureka!')\r\n\r\nif (\"serviceWorker\" in navigator){\r\n    navigator.serviceWorker.register(\"sw.js\")\r\n} //# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvYXBwLmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYWxtb3JhdGFsbGEuZ2l0aHViLmlvLy4vc3JjL2FwcC5qcz8xMTEyIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIZXVyZWthIScpXHJcblxyXG5pZiAoXCJzZXJ2aWNlV29ya2VyXCIgaW4gbmF2aWdhdG9yKXtcclxuICAgIG5hdmlnYXRvci5zZXJ2aWNlV29ya2VyLnJlZ2lzdGVyKFwic3cuanNcIilcclxufSAiXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/app.js\n");
+/******/ })()
+;
---
+++ style-guide.md
@@ -0,0 +1,6 @@
+-- Branding
+
+vdrkblue - hsl(230, 17%, 14%)
+white
+black
+text - hsl(228, 12%, 44%)
